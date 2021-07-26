5ed81e8ff5afb6fa366b3a2e09b0dfb9f94a725f
Nick Troccoli <nicktroccoli28@gmail.com>
2021-07-26 17:52:41 -0400

    Update runLocal.sh to open web browser

--- runLocal.sh
+++ runLocal.sh
@@ -8,5 +8,7 @@ function ctrl_c() {
 	kill -9 %1
 }
 
+python3 compile.py
 python3 -m http.server --directory docs &
+python3 -m webbrowser http://localhost:8000 > /dev/null
 watchmedo auto-restart --directory templates --directory config --recursive python3 compile.py
